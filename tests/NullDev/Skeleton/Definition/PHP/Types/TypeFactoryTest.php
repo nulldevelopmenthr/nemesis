7dc281d3231f5c26e27586325bc00993f0d17efd
Miro Svrtan <miro@mirosvrtan.me>
2017-07-15 23:03:16 +0200

    Add tests to TypeFactoryTest

--- tests/NullDev/Skeleton/Definition/PHP/Types/TypeFactoryTest.php
+++ tests/NullDev/Skeleton/Definition/PHP/Types/TypeFactoryTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace tests\NullDev\Skeleton\Definition\PHP\Types;
 
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\ArrayType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\BoolType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\FloatType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\IntType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\StringType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\TypeDeclaration;
 use NullDev\Skeleton\Definition\PHP\Types\TypeFactory;
 use PHPUnit_Framework_TestCase;
 
 /**
  * @covers \NullDev\Skeleton\Definition\PHP\Types\TypeFactory
- * @group nemesis
+ * @group  unit
  */
 class TypeFactoryTest extends PHPUnit_Framework_TestCase
 {
@@ -21,8 +28,46 @@ class TypeFactoryTest extends PHPUnit_Framework_TestCase
         $this->typeFactory = new TypeFactory();
     }
 
-    public function testNothing(): void
+    /**
+     * @dataProvider provideStaticTypes
+     */
+    public function testStaticTypes(string $name, TypeDeclaration $expectedType): void
     {
-        $this->markTestIncomplete('Auto generated using nemesis');
+        self::assertEquals($expectedType, $this->typeFactory->createFromName($name));
+    }
+
+    public function provideStaticTypes(): array
+    {
+        return [
+            ['string', new StringType()],
+            ['array', new ArrayType()],
+            ['int', new IntType()],
+            ['float', new FloatType()],
+            ['bool', new BoolType()],
+        ];
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testParamsWithoutNameAreNotSupported(): void
+    {
+        $this->typeFactory->createFromName('');
+    }
+
+    /**
+     * @dataProvider provideClassNames
+     */
+    public function testItWillCreateClassTypeForEverythingElse(string $name): void
+    {
+        self::assertInstanceOf(ClassType::class, $this->typeFactory->createFromName($name));
+    }
+
+    public function provideClassNames(): array
+    {
+        return [
+            ['\DateTime'],
+            [self::class],
+        ];
     }
 }
