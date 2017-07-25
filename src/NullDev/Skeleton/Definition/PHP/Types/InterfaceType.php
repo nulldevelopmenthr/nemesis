ce9ce0ce61c39b699113182eac4e8b7fd4212ef9
Miro Svrtan <miro@mirosvrtan.me>
2017-07-25 23:26:54 +0200

    :#48 Define PHP types (class,interface,trait)

---
+++ src/NullDev/Skeleton/Definition/PHP/Types/InterfaceType.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\Definition\PHP\Types;
+
+/**
+ * @see InterfaceTypeSpec
+ * @see InterfaceTypeTest
+ */
+class InterfaceType extends ConceptName
+{
+}
---
+++ tests/NullDev/Skeleton/Definition/PHP/Types/ClassTypeTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Skeleton\Definition\PHP\Types;
+
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Skeleton\Definition\PHP\Types\ClassType
+ * @covers \NullDev\Skeleton\Definition\PHP\Types\ConceptName
+ * @group  unit
+ */
+class ClassTypeTest extends PHPUnit_Framework_TestCase
+{
+    public function testNamespacedInterface(): void
+    {
+        $interfaceType = new ClassType('MyClass', 'Namespace1\Namespace2');
+
+        self::assertEquals('MyClass', $interfaceType->getName());
+        self::assertEquals('Namespace1\Namespace2\MyClass', $interfaceType->getFullName());
+        self::assertEquals('Namespace1\Namespace2', $interfaceType->getNamespace());
+        self::assertTrue($interfaceType->hasNamespace());
+    }
+
+    public function testNonNamespacedInterface(): void
+    {
+        $interfaceType = new ClassType('MyClass');
+
+        self::assertEquals('MyClass', $interfaceType->getName());
+        self::assertEquals('MyClass', $interfaceType->getFullName());
+        self::assertEquals(null, $interfaceType->getNamespace());
+        self::assertFalse($interfaceType->hasNamespace());
+    }
+
+    public function testNamespacedInterfaceCanBeCreatedFromFullyQualifiedClassName(): void
+    {
+        $interfaceType = ClassType::create('Namespace1\Namespace2\MyClass');
+
+        self::assertEquals('MyClass', $interfaceType->getName());
+        self::assertEquals('Namespace1\Namespace2\MyClass', $interfaceType->getFullName());
+        self::assertEquals('Namespace1\Namespace2', $interfaceType->getNamespace());
+        self::assertTrue($interfaceType->hasNamespace());
+    }
+
+    public function testNonNamespacedInterfaceCanBeCreatedFromFullyQualifiedClassName(): void
+    {
+        $interfaceType = ClassType::create('MyClass');
+
+        self::assertEquals('MyClass', $interfaceType->getName());
+        self::assertEquals('MyClass', $interfaceType->getFullName());
+        self::assertEquals(null, $interfaceType->getNamespace());
+        self::assertFalse($interfaceType->hasNamespace());
+    }
+}
