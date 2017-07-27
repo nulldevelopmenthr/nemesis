dc2407f916e8cf1398425f63058cf56e3adee997
Miro Svrtan <miro@mirosvrtan.me>
2017-07-27 20:29:14 +0200

    :#54 Add DocComment

---
+++ tests/NullDev/Skeleton/Definition/PHP/DocCommentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Skeleton\Definition\PHP;
+
+use NullDev\Skeleton\Definition\PHP\DocComment;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Skeleton\Definition\PHP\DocComment
+ * @group unit
+ */
+class DocCommentTest extends PHPUnit_Framework_TestCase
+{
+    /** @var DocComment */
+    private $docComment;
+
+    public function setUp(): void
+    {
+        $this->docComment = new DocComment();
+    }
+
+    public function testNothing(): void
+    {
+        $this->markTestIncomplete('nothing to test');
+    }
+}
