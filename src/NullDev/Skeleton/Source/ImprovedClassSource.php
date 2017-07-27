94e0fc961c14f9dd935ba06ed3b62cc1f576aff4
Miro Svrtan <miro@mirosvrtan.me>
2017-07-27 20:21:23 +0200

    :#54 Add class source

---
+++ src/NullDev/Skeleton/Source/ImprovedClassSource.php
@@ -0,0 +1,281 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\Source;
+
+use NullDev\Skeleton\Definition\PHP\DocComment;
+use NullDev\Skeleton\Definition\PHP\Methods\ConstructorMethod;
+use NullDev\Skeleton\Definition\PHP\Methods\GetterMethod;
+use NullDev\Skeleton\Definition\PHP\Methods\Method;
+use NullDev\Skeleton\Definition\PHP\Parameter;
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use NullDev\Skeleton\Definition\PHP\Types\Importable;
+use NullDev\Skeleton\Definition\PHP\Types\InterfaceType;
+use NullDev\Skeleton\Definition\PHP\Types\TraitType;
+use NullDev\Skeleton\Definition\PHP\Types\Type;
+
+/**
+ * @see ImprovedClassSourceSpec
+ * @see ImprovedClassSourceTest
+ */
+class ImprovedClassSource
+{
+    private $classType;
+    private $docComments = [];
+    private $parent;
+    private $interfaces = [];
+    private $traits     = [];
+    private $constructor;
+
+    private $properties = [];
+    private $methods    = [];
+
+    private $imports = [];
+
+    public function __construct(ClassType $classType)
+    {
+        $this->classType = $classType;
+    }
+
+    public function getClassType(): ClassType
+    {
+        return $this->classType;
+    }
+
+    public function hasNamespace(): bool
+    {
+        return $this->classType->hasNamespace();
+    }
+
+    public function getNamespace(): string
+    {
+        return $this->classType->getNamespace();
+    }
+
+    public function getName(): string
+    {
+        return $this->classType->getName();
+    }
+
+    public function getFullName(): string
+    {
+        return $this->classType->getFullName();
+    }
+
+    //-----   DocComments   -----
+
+    public function addDocComment(DocComment $comment)
+    {
+        $this->docComments[] = $comment;
+    }
+
+    public function getDocComments(): array
+    {
+        return $this->docComments;
+    }
+
+    //-----     Parent     -----
+
+    public function addParent(ClassType $parent)
+    {
+        if ($this->hasParent()) {
+            throw new \Exception('Err 1000100: Class already has parent!');
+        }
+
+        $this->parent = $parent;
+
+        $this->addImportIfEligible($parent);
+
+        return $this;
+    }
+
+    public function hasParent(): bool
+    {
+        if (null === $this->parent) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getParent(): ?ClassType
+    {
+        return $this->parent;
+    }
+
+    public function getParentName(): ?string
+    {
+        if (false === $this->hasParent()) {
+            return null;
+        }
+
+        return $this->getParent()->getName();
+    }
+
+    public function getParentFullName(): ?string
+    {
+        if (false === $this->hasParent()) {
+            return null;
+        }
+
+        return $this->getParent()->getFullName();
+    }
+
+    //-----     Interfaces     -----
+
+    public function addInterface(InterfaceType $interface)
+    {
+        $this->interfaces[] = $interface;
+        $this->addImportIfEligible($interface);
+
+        return $this;
+    }
+
+    public function hasInterfaces(): bool
+    {
+        if (0 === count($this->interfaces)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getInterfaces(): array
+    {
+        return $this->interfaces;
+    }
+
+    //-----     Traits     -----
+
+    public function addTrait(TraitType $trait)
+    {
+        $this->traits[] = $trait;
+        $this->addImportIfEligible($trait);
+
+        return $this;
+    }
+
+    public function hasTraits(): bool
+    {
+        if (0 === count($this->traits)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getTraits(): array
+    {
+        return $this->traits;
+    }
+
+    //-----     ConstructorMethod     -----
+
+    public function hasConstructorMethod(): bool
+    {
+        if (null === $this->constructor) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function addConstructorMethod(ConstructorMethod $constructor)
+    {
+        if ($this->hasConstructorMethod()) {
+            throw new \Exception('Err 1000200: Class already has constructor!');
+        }
+
+        $this->constructor = $constructor;
+
+        foreach ($constructor->getMethodParameters() as $parameter) {
+            if ($parameter->hasType()) {
+                $this->addImportIfEligible($parameter->getType());
+            }
+            $this->addProperty($parameter);
+        }
+
+        $this->addMethod($constructor);
+
+        foreach ($constructor->getMethodParameters() as $parameter) {
+            $this->addGetterMethod($parameter);
+        }
+
+        return $this;
+    }
+
+    public function getConstructorMethod(): ?ConstructorMethod
+    {
+        return $this->constructor;
+    }
+
+    public function getConstructorParameters(): array
+    {
+        if (false === $this->hasConstructorMethod()) {
+            return [];
+        }
+
+        return $this->constructor->getMethodParameters();
+    }
+
+    //-----     Properties     -----
+
+    public function addProperty(Parameter $property)
+    {
+        $this->properties[] = $property;
+    }
+
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    //-----     Methods     -----
+
+    public function addGetterMethod(Parameter $parameter)
+    {
+        $this->addMethod(new GetterMethod($parameter));
+    }
+
+    public function addMethod(Method $method)
+    {
+        $this->methods[] = $method;
+    }
+
+    public function getMethods(): array
+    {
+        return $this->methods;
+    }
+
+    //-----     Import     -----
+
+    public function addImports(Type ...$imports)
+    {
+        foreach ($imports as $import) {
+            $this->addImportIfEligible($import);
+        }
+    }
+
+    public function addImport(Type $import)
+    {
+        $this->addImportIfEligible($import);
+    }
+
+    private function addImportIfEligible(Type $import)
+    {
+        if ($import instanceof Importable && false === in_array($import, $this->imports)) {
+            $this->imports[] = $import;
+        }
+    }
+
+    public function getImports(): array
+    {
+        $sorter = function ($first, $second) {
+            return $first->getFullName() <=> $second->getFullName();
+        };
+        usort($this->imports, $sorter);
+
+        return $this->imports;
+    }
+}
---
+++ tests/NullDev/Skeleton/Source/ImprovedClassSourceTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Skeleton\Source;
+
+use NullDev\Skeleton\Definition\PHP\Methods\ConstructorMethod;
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\ArrayType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\BoolType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\FloatType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\IntType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\StringType;
+use NullDev\Skeleton\Source\ImprovedClassSource;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Skeleton\Source\ImprovedClassSource
+ * @group  nemesis
+ */
+class ImprovedClassSourceTest extends PHPUnit_Framework_TestCase
+{
+    public function testDefaultGetters(): void
+    {
+        $classType           = ClassType::createFromFullyQualified('Vendor\Namespace\Name');
+        $improvedClassSource = new ImprovedClassSource($classType);
+
+        self::assertSame($classType, $improvedClassSource->getClassType());
+        self::assertTrue($improvedClassSource->hasNamespace());
+        self::assertEquals('Vendor\Namespace', $improvedClassSource->getNamespace());
+        self::assertEquals('Name', $improvedClassSource->getName());
+        self::assertEquals('Vendor\Namespace\Name', $improvedClassSource->getFullName());
+        self::assertEmpty($improvedClassSource->getDocComments());
+
+        self::assertFalse($improvedClassSource->hasParent());
+        self::assertNull($improvedClassSource->getParent());
+        self::assertNull($improvedClassSource->getParentName());
+        self::assertNull($improvedClassSource->getParentFullName());
+
+        self::assertFalse($improvedClassSource->hasInterfaces());
+        self::assertEmpty($improvedClassSource->getInterfaces());
+
+        self::assertFalse($improvedClassSource->hasTraits());
+        self::assertEmpty($improvedClassSource->getTraits());
+
+        self::assertFalse($improvedClassSource->hasConstructorMethod());
+        self::assertNull($improvedClassSource->getConstructorMethod());
+        self::assertEmpty($improvedClassSource->getConstructorParameters());
+
+        self::assertEmpty($improvedClassSource->getProperties());
+
+        self::assertEmpty($improvedClassSource->getMethods());
+
+        self::assertEmpty($improvedClassSource->getImports());
+    }
+
+    public function testStaticTypesWillNotBeAddedToImport(): void
+    {
+        $classType           = ClassType::createFromFullyQualified('Vendor\Namespace\Name');
+        $improvedClassSource = new ImprovedClassSource($classType);
+
+        $improvedClassSource->addImport(new StringType());
+        $improvedClassSource->addImport(new IntType());
+        $improvedClassSource->addImport(new ArrayType());
+        $improvedClassSource->addImport(new FloatType());
+        $improvedClassSource->addImport(new BoolType());
+
+        self::assertEmpty($improvedClassSource->getImports());
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testOnlyOneConstructorMethodCanBeAdded(): void
+    {
+        $improvedClassSource = new ImprovedClassSource(ClassType::createFromFullyQualified('Vendor\Namespace\Name'));
+
+        $improvedClassSource->addConstructorMethod(new ConstructorMethod([]));
+        $improvedClassSource->addConstructorMethod(new ConstructorMethod([]));
+    }
+}
