1275f0cd073ddff1498ac8e23c5168d617c32cb9
Miro Svrtan <miro@mirosvrtan.me>
2017-07-29 23:22:10 +0200

    Task #84 Add PHPSpecGenerator tests

--- tests/NullDev/PhpSpecSkeleton/SpecGeneratorTest.php
+++ tests/NullDev/PhpSpecSkeleton/SpecGeneratorTest.php
@@ -5,24 +5,59 @@ declare(strict_types=1);
 namespace tests\NullDev\PhpSpecSkeleton;
 
 use NullDev\PhpSpecSkeleton\SpecGenerator;
-use PHPUnit_Framework_TestCase;
+use NullDev\Skeleton\CodeGenerator\PhpParserGenerator;
+use NullDev\Skeleton\Source\ImprovedClassSource;
+use tests\NullDev\ContainerSupportedTestCase;
+use tests\NullDev\Skeleton\CodeGenerator\SeniorDeveloperProvider;
 
 /**
  * @covers \NullDev\PhpSpecSkeleton\SpecGenerator
- * @group nemesis
+ * @group  codeGeneration
  */
-class SpecGeneratorTest extends PHPUnit_Framework_TestCase
+class SpecGeneratorTest extends ContainerSupportedTestCase
 {
     /** @var SpecGenerator */
     private $specGenerator;
+    /** @var PhpParserGenerator */
+    private $phpParserGenerator;
 
     public function setUp(): void
     {
-        $this->specGenerator = SpecGenerator::default();
+        $this->specGenerator      = $this->getService(SpecGenerator::class);
+        $this->phpParserGenerator = $this->getService(PhpParserGenerator::class);
     }
 
-    public function testNothing(): void
+    /**
+     * @dataProvider provideTestRenderData
+     */
+    public function testNothing(ImprovedClassSource $classSource, string $outputName): void
     {
-        $this->markTestIncomplete('Auto generated using nemesis');
+        $test = $this->specGenerator->generate($classSource);
+
+        $this->assertSame($this->getFileContent($outputName), $this->phpParserGenerator->getOutput($test));
+    }
+
+    public function provideTestRenderData(): array
+    {
+        $provider = new SeniorDeveloperProvider();
+
+        return [
+            [new ImprovedClassSource($provider->provideClassType()), 'class-spec'],
+            [$provider->provideSourceWithParent(), 'class-with-parent-spec'],
+            [$provider->provideSourceWithInterface(), 'class-with-interface-spec'],
+            [$provider->provideSourceWithTrait(), 'class-with-trait-spec'],
+            [$provider->provideSourceWithAll(), 'class-with-all-spec'],
+            [$provider->provideSourceWithAllMulti(), 'class-with-all-multi-spec'],
+            [$provider->provideSourceWithOneParamConstructor(), 'class-with-all-1-param-spec'],
+            [$provider->provideSourceWithTwoParamConstructor(), 'class-with-all-2-param-spec'],
+            [$provider->provideSourceWithThreeParamConstructor(), 'class-with-all-3-param-spec'],
+            [$provider->provideSourceWithOneClasslessParamConstructor(), 'class-with-all-1-classless-param-spec'],
+            [$provider->provideSourceWithOneTypeDeclarationParamConstructor(), 'class-with-all-1-scalartypes-param-spec'],
+        ];
+    }
+
+    protected function getFileContent(string $fileName): string
+    {
+        return file_get_contents(__DIR__.'/example-outputs/'.$fileName.'.output');
     }
 }
