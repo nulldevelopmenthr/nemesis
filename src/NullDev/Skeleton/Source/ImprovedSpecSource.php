d2d7215dd1d820dc81d70f1b8f4c3aba061b7aa4
Miro Svrtan <miro@mirosvrtan.me>
2017-07-30 04:39:51 +0200

    Task #94 Add ImprovedTestSource & ImprovedSpecSource

---
+++ src/NullDev/Skeleton/Source/ImprovedSpecSource.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\Source;
+
+/**
+ * @see ImprovedSpecSourceSpec
+ * @see ImprovedSpecSourceTest
+ */
+class ImprovedSpecSource extends ImprovedClassSource
+{
+}
---
+++ src/NullDev/Skeleton/Source/ImprovedTestSource.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\Source;
+
+/**
+ * @see ImprovedTestSourceSpec
+ * @see ImprovedTestSourceTest
+ */
+class ImprovedTestSource extends ImprovedClassSource
+{
+}
