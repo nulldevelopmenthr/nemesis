9c97c2b31dc385d2cb608b411425387be36d51e8
Miro Svrtan <miro@mirosvrtan.me>
2017-08-04 22:39:13 +0200

    remove supress warnings

--- src/NullDev/Skeleton/Uuid/Cli/Uuid4IdentityCommand.php
+++ src/NullDev/Skeleton/Uuid/Cli/Uuid4IdentityCommand.php
@@ -14,7 +14,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  * @codeCoverageIgnore
  */
 class Uuid4IdentityCommand extends SimpleSkeletonGeneratorCommand
