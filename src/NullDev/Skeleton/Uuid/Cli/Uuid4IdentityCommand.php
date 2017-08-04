ea81e2561a5b7fe1a067c9016973226fc24edfa9
Miro Svrtan <miro@mirosvrtan.me>
2017-08-04 22:35:57 +0200

    Remove unused method from Uuid4IdentityCommand

--- src/NullDev/Skeleton/Uuid/Cli/Uuid4IdentityCommand.php
+++ src/NullDev/Skeleton/Uuid/Cli/Uuid4IdentityCommand.php
@@ -7,8 +7,6 @@ namespace NullDev\Skeleton\Uuid\Cli;
 use NullDev\Skeleton\CodeGenerator\PhpParserGenerator;
 use NullDev\Skeleton\Command\SimpleSkeletonGeneratorCommand;
 use NullDev\Skeleton\Definition\PHP\Types\ClassType;
-use NullDev\Skeleton\Source\ImprovedClassSource;
-use NullDev\Skeleton\SourceFactory\Uuid4IdentitySourceFactory;
 use NullDev\Skeleton\Uuid\Command\CreateUuidClass;
 use NullDev\Skeleton\Uuid\Handler\CreateUuidClassHandler;
 use Symfony\Component\Console\Input\InputInterface;
@@ -50,11 +48,6 @@ class Uuid4IdentityCommand extends SimpleSkeletonGeneratorCommand
     protected function getSources(CreateUuidClass $command): array
     {
         return $this->getService(CreateUuidClassHandler::class)->handle($command);
-    }
-
-    protected function getSource(ClassType $classType): ImprovedClassSource
-    {
-        return $this->getService(Uuid4IdentitySourceFactory::class)->create($classType);
     }
 
     protected function getSectionMessage(): string
