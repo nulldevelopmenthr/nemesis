6ea55ac66c48ad9bcb7f380993d00882f6d5e1ac
Miro Svrtan <miro@mirosvrtan.me>
2017-08-06 13:47:17 +0200

    Add LolProvider and use it

---
+++ tests/NullDev/Skeleton/CodeGenerator/LolProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Skeleton\CodeGenerator;
+
+use NullDev\Skeleton\Definition\PHP\Methods\ConstructorMethod;
+use NullDev\Skeleton\Definition\PHP\Parameter;
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\ArrayType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\BoolType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\FloatType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\IntType;
+use NullDev\Skeleton\Definition\PHP\Types\TypeDeclaration\StringType;
+use NullDev\Skeleton\Source\ImprovedClassSource;
+
+class LolProvider
+{
+    public function provideAll(): ImprovedClassSource
+    {
+        $classType = ClassType::createFromFullyQualified('BlaBla\Lol');
+
+        $source = new ImprovedClassSource($classType);
+        $source->addConstructorMethod(
+            new ConstructorMethod(
+                [
+                    new Parameter('id', new IntType()),
+                    new Parameter('name', new StringType()),
+                    new Parameter('price', new FloatType()),
+                    new Parameter('smart', new BoolType()),
+                    new Parameter('tags', new ArrayType()),
+                    new Parameter('randomValue'),
+                ]
+            )
+        );
+
+        return $source;
+    }
+}
