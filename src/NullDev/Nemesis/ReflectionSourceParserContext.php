f7263384a66e53e0fa832b673e89f7c8ed3f8f75
Miro Svrtan <miro@mirosvrtan.me>
2017-08-12 19:54:19 +0200

    TASK #137 Add reflection based source parser

---
+++ src/NullDev/Nemesis/ReflectionSourceParserContext.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Nemesis;
+
+class ReflectionSourceParserContext extends SourceParserContext
+{
+    /**
+     * @When I parse it
+     */
+    public function iParseIt()
+    {
+        $this->sourceParser = new ReflectionSourceParser();
+
+        eval($this->input);
+        $this->result = $this->sourceParser->parse('<?php '.$this->input);
+    }
+}
