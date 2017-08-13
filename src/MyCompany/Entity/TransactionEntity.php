66bdfca6d97cdc647c432a41d02ec5c63331335c
Miro Svrtan <miro@mirosvrtan.me>
2017-08-13 12:53:57 +0200

    TASK #143 Add sample classes for testing generation of tests
    In order to test phpunit files generation
    For developers ,
    We will create some classes under "MyCompany" namespace
    Whereas currently we used manually built ImprovedClassSource files

---
+++ src/MyCompany/Entity/TransactionEntity.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyCompany\Entity;
+
+/**
+ * Represents class with getters and setters.
+ */
+class TransactionEntity
+{
+    /** @var string */
+    private $id;
+    /** @var int */
+    private $amount;
+    /** @var \DateTime */
+    private $createdAt;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
