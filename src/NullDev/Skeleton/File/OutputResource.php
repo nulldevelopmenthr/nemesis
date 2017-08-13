77cdc3309061fcaa6598e444e1bbc1f716488b26
Miro Svrtan <miro@mirosvrtan.me>
2017-08-13 19:10:04 +0200

    TASK #182 Define OutputResource

---
+++ src/NullDev/Skeleton/File/OutputResource.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\File;
+
+use NullDev\Skeleton\Source\ImprovedClassSource;
+
+/**
+ * @see OutputResourceSpec
+ * @see OutputResourceTest
+ */
+class OutputResource
+{
+    /** @var string */
+    private $fileName;
+    /** @var ImprovedClassSource */
+    private $classSource;
+    /** @var string */
+    private $output;
+
+    public function __construct(string $fileName, ImprovedClassSource $classSource, string $output)
+    {
+        $this->fileName    = $fileName;
+        $this->classSource = $classSource;
+        $this->output      = $output;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+
+    public function getClassSource(): ImprovedClassSource
+    {
+        return $this->classSource;
+    }
+
+    public function getOutput(): string
+    {
+        return $this->output;
+    }
+}
