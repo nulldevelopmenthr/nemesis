683fd6ca5612c4decbf76f86dc45893e8f1316d7
Miro Svrtan <miro@mirosvrtan.me>
2017-08-14 17:47:29 +0200

    TASK #203 Add NamingStrategyFactory

---
+++ spec/NullDev/Theater/NamingStrategy/NamingStrategyFactorySpec.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\NullDev\Theater\NamingStrategy;
+
+use NullDev\Theater\BoundedContext\ContextName;
+use NullDev\Theater\BoundedContext\ContextNamespace;
+use NullDev\Theater\NamingStrategy\NamingStrategyFactory;
+use NullDev\Theater\NamingStrategy\TheaterNamingStrategy;
+use PhpSpec\ObjectBehavior;
+
+class NamingStrategyFactorySpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith();
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(NamingStrategyFactory::class);
+    }
+
+    public function it_will_create_theater_naming_strategy_instance(
+        ContextName $contextName,
+        ContextNamespace $contextNamespace
+    ) {
+        $this->theater($contextName, $contextNamespace)
+            ->shouldReturnAnInstanceOf(TheaterNamingStrategy::class);
+    }
+}
---
+++ src/NullDev/Theater/NamingStrategy/NamingStrategyFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Theater\NamingStrategy;
+
+use NullDev\Theater\BoundedContext\ContextName;
+use NullDev\Theater\BoundedContext\ContextNamespace;
+
+/**
+ * @see NamingStrategyFactorySpec
+ * @see NamingStrategyFactoryTest
+ */
+class NamingStrategyFactory
+{
+    public function theater(ContextName $contextName, ContextNamespace $contextNamespace): TheaterNamingStrategy
+    {
+        return new TheaterNamingStrategy($contextName, $contextNamespace);
+    }
+}
