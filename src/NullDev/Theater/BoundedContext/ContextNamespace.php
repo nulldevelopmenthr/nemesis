97fd42e4753aab603607b03432c7440a790af0ee
Miro Svrtan <miro@mirosvrtan.me>
2017-08-14 15:31:30 +0200

    TASK #200 Define ContextNamespace

---
+++ src/NullDev/Theater/BoundedContext/ContextNamespace.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Theater\BoundedContext;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @see ContextNamespaceSpec
+ * @see ContextNamespaceTest
+ */
+class ContextNamespace
+{
+    /** @var string */
+    private $namespace;
+
+    public function __construct(string $namespace)
+    {
+        Assert::notEmpty($namespace, 'Namespace should not be empty.');
+
+        Assert::true(substr($namespace, -1) === '\\', 'Namespace must end with \\.');
+
+        $this->namespace = $namespace;
+    }
+
+    public function getValue(): string
+    {
+        return $this->namespace;
+    }
+
+    public function __toString(): string
+    {
+        return $this->namespace;
+    }
+}
---
+++ tests/NullDev/Theater/BoundedContext/ContextNamespaceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Theater\BoundedContext;
+
+use Exception;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDev\Theater\BoundedContext\ContextNamespace;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Theater\BoundedContext\ContextNamespace
+ * @group todo
+ */
+class ContextNamespaceTest extends PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var string */
+    private $namespace;
+    /** @var ContextNamespace */
+    private $contextNamespace;
+
+    public function setUp()
+    {
+        $this->namespace        = 'MyCompany\SecretProject\Namespace\\';
+        $this->contextNamespace = new ContextNamespace($this->namespace);
+    }
+
+    public function testGetValue()
+    {
+        self::assertEquals($this->namespace, $this->contextNamespace->getValue());
+    }
+
+    public function testToString()
+    {
+        self::assertEquals($this->namespace, $this->contextNamespace->__toString());
+    }
+
+    /**
+     * @dataProvider provideBadContextNamespaces
+     */
+    public function testDoesntAcceptName(string $name, string $expectedExceptionMessage)
+    {
+        self::expectException(Exception::class);
+        self::expectExceptionMessage($expectedExceptionMessage);
+
+        new ContextNamespace($name);
+    }
+
+    public function provideBadContextNamespaces(): array
+    {
+        return [
+            ['', 'Namespace should not be empty.'],
+            ['Something\User', 'Namespace must end with \.'],
+        ];
+    }
+}
