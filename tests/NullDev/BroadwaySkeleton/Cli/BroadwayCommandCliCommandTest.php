27575a7e0c2b41ce32aafc77f5ffe2c578a5e719
Miro Svrtan <miro@mirosvrtan.me>
2017-08-14 20:11:40 +0200

    Empty tests for CLI commands

---
+++ tests/NullDev/BroadwaySkeleton/Cli/BroadwayCommandCliCommandTest.php
@@ -0,0 +1,188 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\BroadwaySkeleton\Cli;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDev\BroadwaySkeleton\Cli\BroadwayCommandCliCommand;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\BroadwaySkeleton\Cli\BroadwayCommandCliCommand
+ * @group  todo
+ */
+class BroadwayCommandCliCommandTest extends PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var  */
+    private $name;
+    /** @var BroadwayCommandCliCommand */
+    private $broadwayCommandCliCommand;
+
+    public function setUp()
+    {
+        $this->name                      = 'name';
+        $this->broadwayCommandCliCommand = new BroadwayCommandCliCommand($this->name);
+    }
+
+    public function testIgnoreValidationErrors()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetApplication()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetHelperSet()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetHelperSet()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetApplication()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testIsEnabled()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testRun()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetCode()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testMergeApplicationDefinition()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetDefinition()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetDefinition()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetNativeDefinition()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testAddArgument()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testAddOption()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetName()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetProcessTitle()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetName()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetHidden()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testIsHidden()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetDescription()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetDescription()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetHelp()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetHelp()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetProcessedHelp()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetAliases()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetAliases()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetSynopsis()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testAddUsage()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetUsages()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetHelper()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testSetContainer()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+
+    public function testGetService()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+}
