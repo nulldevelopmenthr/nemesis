32ae9a8261a281a16ac99ff02ebce09608216487
Miro Svrtan <miro@mirosvrtan.me>
2017-08-14 19:52:55 +0200

    TASK #199 Add BoundedContextConfig

---
+++ tests/NullDev/Theater/BoundedContext/BoundedContextConfigTest.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Theater\BoundedContext;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use NullDev\Theater\BoundedContext\BoundedContextConfig;
+use NullDev\Theater\BoundedContext\ContextName;
+use NullDev\Theater\BoundedContext\ContextNamespace;
+use NullDev\Theater\Naming\Aggregate\EntityClassName;
+use NullDev\Theater\Naming\Aggregate\RootIdClassName;
+use NullDev\Theater\Naming\Aggregate\RootModelClassName;
+use NullDev\Theater\Naming\Aggregate\RootRepositoryClassName;
+use NullDev\Theater\Naming\CommandClassName;
+use NullDev\Theater\Naming\CommandHanderClassName;
+use NullDev\Theater\Naming\EventClassName;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Theater\BoundedContext\BoundedContextConfig
+ * @group  unit
+ */
+class BoundedContextConfigTest extends PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var MockInterface|ContextName */
+    private $name;
+    /** @var MockInterface|ContextNamespace */
+    private $namespace;
+    /** @var MockInterface|RootIdClassName */
+    private $rootIdClassName;
+    /** @var MockInterface|RootModelClassName */
+    private $modelClassName;
+    /** @var MockInterface|RootRepositoryClassName */
+    private $repositoryClassName;
+    /** @var MockInterface|CommandHanderClassName */
+    private $commandHanderClassName;
+    /** @var BoundedContextConfig */
+    private $config;
+
+    public function setUp()
+    {
+        $this->name                   = Mockery::mock(ContextName::class);
+        $this->namespace              = Mockery::mock(ContextNamespace::class);
+        $this->rootIdClassName        = Mockery::mock(RootIdClassName::class);
+        $this->modelClassName         = Mockery::mock(RootModelClassName::class);
+        $this->repositoryClassName    = Mockery::mock(RootRepositoryClassName::class);
+        $this->commandHanderClassName = Mockery::mock(CommandHanderClassName::class);
+        $this->config                 = new BoundedContextConfig(
+            $this->name,
+            $this->namespace,
+            $this->rootIdClassName,
+            $this->modelClassName,
+            $this->repositoryClassName,
+            $this->commandHanderClassName
+        );
+    }
+
+    public function testGetName()
+    {
+        self::assertEquals($this->name, $this->config->getName());
+    }
+
+    public function testGetNamespace()
+    {
+        self::assertEquals($this->namespace, $this->config->getNamespace());
+    }
+
+    public function testGetRootIdClassName()
+    {
+        self::assertEquals($this->rootIdClassName, $this->config->getRootIdClassName());
+    }
+
+    public function testGetModelClassName()
+    {
+        self::assertEquals($this->modelClassName, $this->config->getModelClassName());
+    }
+
+    public function testGetRepositoryClassName()
+    {
+        self::assertEquals($this->repositoryClassName, $this->config->getRepositoryClassName());
+    }
+
+    public function testGetCommandHanderClassName()
+    {
+        self::assertEquals($this->commandHanderClassName, $this->config->getCommandHanderClassName());
+    }
+
+    public function testGetEntityClassNames()
+    {
+        self::assertEmpty($this->config->getEntityClassNames());
+    }
+
+    public function testGetCommandClassNames()
+    {
+        self::assertEmpty($this->config->getCommandClassNames());
+    }
+
+    public function testGetEventClassNames()
+    {
+        self::assertEmpty($this->config->getEventClassNames());
+    }
+
+    public function testAddEntity()
+    {
+        $entity = Mockery::mock(EntityClassName::class);
+
+        $this->config->addEntity($entity);
+        self::assertEquals([$entity], $this->config->getEntityClassNames());
+    }
+
+    public function testAddCommand()
+    {
+        $command = Mockery::mock(CommandClassName::class);
+
+        $this->config->addCommand($command);
+        self::assertEquals([$command], $this->config->getCommandClassNames());
+    }
+
+    public function testAddEvent()
+    {
+        $event = Mockery::mock(EventClassName::class);
+
+        $this->config->addEvent($event);
+        self::assertEquals([$event], $this->config->getEventClassNames());
+    }
+}
