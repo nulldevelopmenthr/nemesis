0b1dda0114ef37db1bfc39027c74e119bfa915ee
Miro Svrtan <miro@mirosvrtan.me>
2017-08-14 17:46:54 +0200

    TASK #203 Define NamingStrategy interface

---
+++ src/NullDev/Theater/NamingStrategy/NamingStrategy.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Theater\NamingStrategy;
+
+use NullDev\Theater\Naming\Aggregate\EntityClassName;
+use NullDev\Theater\Naming\Aggregate\RootIdClassName;
+use NullDev\Theater\Naming\Aggregate\RootModelClassName;
+use NullDev\Theater\Naming\Aggregate\RootRepositoryClassName;
+use NullDev\Theater\Naming\CommandClassName;
+use NullDev\Theater\Naming\CommandHanderClassName;
+use NullDev\Theater\Naming\EventClassName;
+
+interface NamingStrategy
+{
+    public function aggregateRootId(): RootIdClassName;
+
+    public function aggregateRootModel(): RootModelClassName;
+
+    public function aggregateRootRepository(): RootRepositoryClassName;
+
+    public function aggregateEntity(string $entityName): EntityClassName;
+
+    public function commandHandler(): CommandHanderClassName;
+
+    public function command(string $commandName): CommandClassName;
+
+    public function event(string $eventName): EventClassName;
+}
