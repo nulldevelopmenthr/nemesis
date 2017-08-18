fa748206ebad06d1d08dbb14aed9c96e4ebcc2a5
Miro Svrtan <miro@mirosvrtan.me>
2017-08-19 00:06:58 +0200

    TASK #222 AggregateRootId command & handler

---
+++ src/NullDev/BroadwaySkeleton/Handler/BroadwayAggregateRootIdHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\BroadwaySkeleton\Handler;
+
+use NullDev\BroadwaySkeleton\Command\CreateBroadwayAggregateRootId;
+use NullDev\Skeleton\Uuid\SourceFactory\Uuid4IdentitySourceFactory;
+
+/**
+ * @see BroadwayAggregateRootIdHandlerSpec
+ * @see BroadwayAggregateRootIdHandlerTest
+ */
+class BroadwayAggregateRootIdHandler
+{
+    /** @var Uuid4IdentitySourceFactory */
+    private $uuid4IdentitySourceFactory;
+
+    public function __construct(Uuid4IdentitySourceFactory $uuid4IdentitySourceFactory)
+    {
+        $this->uuid4IdentitySourceFactory  = $uuid4IdentitySourceFactory;
+    }
+
+    public function handleCreateBroadwayAggregateRootId(CreateBroadwayAggregateRootId $command): array
+    {
+        $classes = [
+            $this->uuid4IdentitySourceFactory->create($command->getRootIdClassName()),
+        ];
+
+        return $classes;
+    }
+}
