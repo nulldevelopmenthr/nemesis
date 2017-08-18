7fc76f98465f08ac17c87a2fb7b4984fdbd24ab8
Miro Svrtan <miro@mirosvrtan.me>
2017-08-18 18:32:16 +0200

    FEAT #171 Add Property definition

---
+++ src/NullDev/Skeleton/Definition/PHP/Property.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDev\Skeleton\Definition\PHP;
+
+use NullDev\Skeleton\Definition\PHP\Types\Type;
+use NullDev\Skeleton\Definition\PHP\Types\TypeFactory;
+
+/**
+ * @see PropertySpec
+ * @see PropertyTest
+ */
+class Property
+{
+    /** @var string */
+    private $name;
+    /** @var Type|null */
+    private $type;
+
+    public function __construct(string $name, Type $type = null)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    public static function create(string $name, ?string $type = null): Property
+    {
+        return new self($name, TypeFactory::create($type));
+    }
+
+    public static function createFromParameter(Parameter $parameter): Property
+    {
+        return new self($parameter->getName(), $parameter->getType());
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function getTypeShortName(): string
+    {
+        if (null === $this->type) {
+            throw new \Exception();
+        }
+
+        return $this->type->getName();
+    }
+
+    public function hasType(): bool
+    {
+        if (null === $this->type) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/NullDev/Skeleton/Definition/PHP/PropertyTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\NullDev\Skeleton\Definition\PHP;
+
+use NullDev\Skeleton\Definition\PHP\Parameter;
+use NullDev\Skeleton\Definition\PHP\Property;
+use NullDev\Skeleton\Definition\PHP\Types\ClassType;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \NullDev\Skeleton\Definition\PHP\Property
+ * @group  unit
+ */
+class PropertyTest extends PHPUnit_Framework_TestCase
+{
+    public function testItWorksWithType(): void
+    {
+        $type = ClassType::createFromFullyQualified('Vendor\Namespace\MyClass');
+
+        $property = new Property('name', $type);
+
+        self::assertEquals('name', $property->getName());
+        self::assertEquals($type, $property->getType());
+        self::assertEquals('MyClass', $property->getTypeShortName());
+        self::assertTrue($property->hasType());
+    }
+
+    public function testItWorksWithoutType(): void
+    {
+        $property = Property::create('name');
+        self::assertEquals('name', $property->getName());
+        self::assertNull($property->getType());
+        self::assertFalse($property->hasType());
+    }
+
+    /** @expectedException \Throwable */
+    public function testItThrowsExceptionWithoutTypeOnCallingGetTypeShortName(): void
+    {
+        $property = Property::create('name');
+        $property->getTypeShortName();
+    }
+
+    /** @dataProvider provideParameters */
+    public function testItCanCreatePropertyFromParameter(Parameter $parameter)
+    {
+        $property = Property::createFromParameter($parameter);
+
+        self::assertInstanceOf(Property::class, $property);
+        self::assertEquals($parameter->getName(), $property->getName());
+        self::assertEquals($parameter->getType(), $property->getType());
+    }
+
+    public function provideParameters(): array
+    {
+        return [
+            [Parameter::create('name')],
+            [Parameter::create('name', 'string')],
+            [Parameter::create('name', 'DateTime')],
+        ];
+    }
+}
