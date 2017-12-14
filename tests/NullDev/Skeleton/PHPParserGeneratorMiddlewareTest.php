7c039e4b171696320c30705ca6a33f3ed341653f
Miro Svrtan <miro@mirosvrtan.me>
2017-12-14 22:43:40 +0100

    Rename PhpParserGeneratorMiddleware-> PHPParserGeneratorMiddleware

--- tests/NullDev/Skeleton/PHPParserGeneratorMiddlewareTest.php
+++ tests/NullDev/Skeleton/PHPParserGeneratorMiddlewareTest.php
@@ -7,28 +7,28 @@ use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery\MockInterface;
 use NullDev\Skeleton\CodeGenerator\PhpParserGenerator;
 use NullDev\Skeleton\File\FileFactory;
-use NullDev\Skeleton\PhpParserGeneratorMiddleware;
+use NullDev\Skeleton\PHPParserGeneratorMiddleware;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \NullDev\Skeleton\PhpParserGeneratorMiddleware
+ * @covers \NullDev\Skeleton\PHPParserGeneratorMiddleware
  * @group  todo
  */
-class PhpParserGeneratorMiddlewareTest extends TestCase
+class PHPParserGeneratorMiddlewareTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
     /** @var MockInterface|PhpParserGenerator */
     private $codeGenerator;
     /** @var MockInterface|FileFactory */
     private $fileFactory;
-    /** @var PhpParserGeneratorMiddleware */
+    /** @var PHPParserGeneratorMiddleware */
     private $sut;
 
     public function setUp()
     {
         $this->codeGenerator = Mockery::mock(PhpParserGenerator::class);
         $this->fileFactory   = Mockery::mock(FileFactory::class);
-        $this->sut           = new PhpParserGeneratorMiddleware($this->codeGenerator, $this->fileFactory);
+        $this->sut           = new PHPParserGeneratorMiddleware($this->codeGenerator, $this->fileFactory);
     }
 
     public function testExecute()
