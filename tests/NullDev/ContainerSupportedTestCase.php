adc87a9578fda40c5e5f292b4c844f9478718bfe
Miro Svrtan <miro@mirosvrtan.me>
2017-12-14 22:35:41 +0100

    Update all references to Application

--- tests/NullDev/ContainerSupportedTestCase.php
+++ tests/NullDev/ContainerSupportedTestCase.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace tests\NullDev;
 
-use NullDev\Nemesis\Application;
+use App\Application;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
