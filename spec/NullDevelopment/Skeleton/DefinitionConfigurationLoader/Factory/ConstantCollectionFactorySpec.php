ed83fc369d55495c64badf40f7c2c3168cc19a01
Miro Svrtan <miro@mirosvrtan.me>
2017-12-19 18:51:51 +0100

    299: Add ConstantCollectionFactory

---
+++ spec/NullDevelopment/Skeleton/DefinitionConfigurationLoader/Factory/ConstantCollectionFactorySpec.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\NullDevelopment\Skeleton\DefinitionConfigurationLoader\Factory;
+
+use NullDevelopment\PhpStructure\DataType\Constant;
+use NullDevelopment\Skeleton\DefinitionConfigurationLoader\Factory\ConstantCollectionFactory;
+use PhpSpec\ObjectBehavior;
+
+class ConstantCollectionFactorySpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith();
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ConstantCollectionFactory::class);
+    }
+
+    public function it_will_convert_list_of_constant_list_to_constant_collection()
+    {
+        $input = [
+            'SOMETHING' => 123,
+            'NAME'      => 'some string',
+            'TAX'       => 2.5,
+            'VALUABLE'  => true,
+        ];
+
+        $resultCollection = $this->create($input);
+
+        foreach ($resultCollection as $result) {
+            $result->shouldBeAnInstanceOf(Constant::class);
+        }
+    }
+}
---
+++ tests/NullDevelopment/Skeleton/DefinitionConfigurationLoader/Factory/ConstantCollectionFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests\NullDevelopment\Skeleton\DefinitionConfigurationLoader\Factory;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDevelopment\PhpStructure\DataType\Constant;
+use NullDevelopment\Skeleton\DefinitionConfigurationLoader\Factory\ConstantCollectionFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\Skeleton\DefinitionConfigurationLoader\Factory\ConstantCollectionFactory
+ * @group  unit
+ */
+class ConstantCollectionFactoryTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var ConstantCollectionFactory */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = new ConstantCollectionFactory();
+    }
+
+    public function testCreate()
+    {
+        $input = [
+            'SOMETHING' => 123,
+            'NAME'      => 'some string',
+            'TAX'       => 2.5,
+            'VALUABLE'  => true,
+        ];
+
+        $resultCollection = $this->sut->create($input);
+
+        self::assertCount(4, $resultCollection);
+
+        self::assertContainsOnlyInstancesOf(Constant::class, $resultCollection);
+    }
+}
