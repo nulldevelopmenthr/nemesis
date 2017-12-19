6112d0f9f81efd10ccc0a2361d1a326948898b64
Miro Svrtan <miro@mirosvrtan.me>
2017-12-19 18:50:37 +0100

    Task #305: Add ClassName::create()

--- src/NullDevelopment/PhpStructure/DataTypeName/ClassName.php
+++ src/NullDevelopment/PhpStructure/DataTypeName/ClassName.php
@@ -10,4 +10,17 @@ namespace NullDevelopment\PhpStructure\DataTypeName;
  */
 class ClassName extends AbstractDataTypeName implements ContractName, Importable
 {
+    public static function create(string $fullName): self
+    {
+        $parts = explode(self::NAMESPACE_SEPARATOR, $fullName);
+        $name  = array_pop($parts);
+
+        $namespace = null;
+
+        if (count($parts) > 0) {
+            $namespace = implode(self::NAMESPACE_SEPARATOR, $parts);
+        }
+
+        return new static($name, $namespace);
+    }
 }
