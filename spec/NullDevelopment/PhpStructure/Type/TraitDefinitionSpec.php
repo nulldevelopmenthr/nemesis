96825b600e29f12a8a33a3a2cf58ea7b2c39f68f
Miro Svrtan <miro@mirosvrtan.me>
2017-12-19 18:51:07 +0100

    Task #305: Add TraitDefinition

---
+++ spec/NullDevelopment/PhpStructure/Type/TraitDefinitionSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\NullDevelopment\PhpStructure\Type;
+
+use NullDevelopment\PhpStructure\Behaviour\Method;
+use NullDevelopment\PhpStructure\DataType\Constant;
+use NullDevelopment\PhpStructure\DataType\Property;
+use NullDevelopment\PhpStructure\DataTypeName\TraitName;
+use NullDevelopment\PhpStructure\Type\TraitDefinition;
+use PhpSpec\ObjectBehavior;
+
+class TraitDefinitionSpec extends ObjectBehavior
+{
+    public function let(
+        TraitName $name,
+        TraitName $trait1,
+        Constant $constant1,
+        Property $property1,
+        Method $method1
+    ) {
+        $this->beConstructedWith($name, [$trait1], [$constant1], [$property1], [$method1]);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(TraitDefinition::class);
+    }
+
+    public function it_exposes_trait_name(TraitName $name)
+    {
+        $this->getName()->shouldReturn($name);
+    }
+
+    public function it_exposes_traits_that_this_trait_uses(TraitName $trait1)
+    {
+        $this->getTraits()->shouldReturn([$trait1]);
+    }
+
+    public function it_exposes_constants_trait_defines(Constant $constant1)
+    {
+        $this->getConstants()->shouldReturn([$constant1]);
+    }
+
+    public function it_exposes_properties_trait_defines(Property $property1)
+    {
+        $this->getProperties()->shouldReturn([$property1]);
+    }
+
+    public function it_exposes_methods_trait_defines(Method $method1)
+    {
+        $this->getMethods()->shouldReturn([$method1]);
+    }
+}
---
+++ tests/NullDevelopment/PhpStructure/Type/TraitDefinitionTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tests\NullDevelopment\PhpStructure\Type;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use NullDevelopment\PhpStructure\Behaviour\Method;
+use NullDevelopment\PhpStructure\DataType\Constant;
+use NullDevelopment\PhpStructure\DataType\Property;
+use NullDevelopment\PhpStructure\DataTypeName\TraitName;
+use NullDevelopment\PhpStructure\Type\TraitDefinition;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\PhpStructure\Type\TraitDefinition
+ * @group  unit
+ */
+class TraitDefinitionTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var MockInterface|TraitName */
+    private $name;
+    /** @var array */
+    private $traits;
+    /** @var array */
+    private $constants;
+    /** @var array */
+    private $properties;
+    /** @var array */
+    private $methods;
+    /** @var TraitDefinition */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->name       = Mockery::mock(TraitName::class);
+        $this->traits     = [Mockery::mock(TraitName::class)];
+        $this->constants  = [Mockery::mock(Constant::class)];
+        $this->properties = [Mockery::mock(Property::class)];
+        $this->methods    = [Mockery::mock(Method::class)];
+        $this->sut        = new TraitDefinition(
+            $this->name, $this->traits, $this->constants, $this->properties, $this->methods
+        );
+    }
+
+    public function testGetName()
+    {
+        self::assertEquals($this->name, $this->sut->getName());
+    }
+
+    public function testGetTraits()
+    {
+        self::assertEquals($this->traits, $this->sut->getTraits());
+    }
+
+    public function testGetConstants()
+    {
+        self::assertEquals($this->constants, $this->sut->getConstants());
+    }
+
+    public function testGetProperties()
+    {
+        self::assertEquals($this->properties, $this->sut->getProperties());
+    }
+
+    public function testGetMethods()
+    {
+        self::assertEquals($this->methods, $this->sut->getMethods());
+    }
+}
