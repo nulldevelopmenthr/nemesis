4969acdda7863ac4623123dbf0fd4f9128211e54
Miro Svrtan <miro@mirosvrtan.me>
2017-12-19 12:20:01 +0100

    Task #300: Add ConstantName

---
+++ tests/NullDevelopment/PhpStructure/DataTypeName/ConstantNameTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace tests\NullDevelopment\PhpStructure\DataTypeName;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDevelopment\PhpStructure\DataTypeName\ConstantName;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\PhpStructure\DataTypeName\ConstantName
+ * @group  unit
+ */
+class ConstantNameTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var string */
+    private $name;
+    /** @var ConstantName */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->name = 'name';
+        $this->sut  = new ConstantName($this->name);
+    }
+
+    public function testGetName()
+    {
+        self::assertEquals($this->name, $this->sut->getName());
+    }
+
+    public function testToString()
+    {
+        self::assertEquals($this->name, $this->sut->__toString());
+    }
+}
