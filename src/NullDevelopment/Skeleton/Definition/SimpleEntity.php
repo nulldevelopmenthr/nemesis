798bcd73d804d252afdca1f2c9589d5ec385db65
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 16:36:32 +0100

    Add some base definitons

---
+++ src/NullDevelopment/Skeleton/Definition/SimpleEntity.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton\Definition;
+
+use NullDevelopment\PhpStructure\Type\ClassType;
+use NullDevelopment\Skeleton\SourceCode;
+
+/**
+ * @see SimpleEntitySpec
+ * @see SimpleEntityTest
+ * @see SimpleEntityLoader
+ * @see SimpleEntityNetteGenerator
+ */
+class SimpleEntity extends ClassType implements SourceCode
+{
+}
