ac893c9f6561213d6be5ff9dcceea8a5ae07d22c
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 16:34:52 +0100

    Generate outputs for nette generator in PHPParserGeneratorMiddleware

--- src/NullDev/Skeleton/PHPParserGeneratorMiddleware.php
+++ src/NullDev/Skeleton/PHPParserGeneratorMiddleware.php
@@ -6,9 +6,12 @@ namespace NullDev\Skeleton;
 
 use Exception;
 use League\Tactician\Middleware;
+use Nette\PhpGenerator\PhpNamespace;
 use NullDev\Skeleton\CodeGenerator\PhpParserGenerator;
 use NullDev\Skeleton\File\FileFactory;
+use NullDev\Skeleton\File\OutputResource2;
 use NullDev\Skeleton\Source\ImprovedClassSource;
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
 
 class PHPParserGeneratorMiddleware implements Middleware
 {
@@ -32,6 +35,17 @@ class PHPParserGeneratorMiddleware implements Middleware
         foreach ($returnValue as $item) {
             if ($item instanceof ImprovedClassSource) {
                 $outputs[] = $this->fileFactory->createOutputResource($item, $this->codeGenerator->getOutput($item));
+            } elseif ($item instanceof PhpNamespace) {
+                $namespaceName = $item->getName();
+                foreach (array_keys($item->getClasses()) as $className) {
+                    $zz = '<?php'.PHP_EOL.PHP_EOL.'declare(strict_types=1);'.PHP_EOL.PHP_EOL.str_replace("\t", '    ', $item);
+
+                    $outputs[] = new OutputResource2(
+                        $this->fileFactory->getPath2(new ClassName($className, $namespaceName)),
+                        new ClassName($className, $namespaceName),
+                        $zz
+                    );
+                }
             } else {
                 throw new Exception('ERR 32242398: Got '.get_class($item).' expected instance of ImprovedClassSource');
             }
