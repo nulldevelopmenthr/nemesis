7634e2b866fd56c6cc14faec3e76b94b28c197b9
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 21:33:04 +0100

    Task #310: Add missing getter to TraitDefinition

--- src/NullDevelopment/PhpStructure/Type/TraitDefinition.php
+++ src/NullDevelopment/PhpStructure/Type/TraitDefinition.php
@@ -8,12 +8,13 @@ use NullDevelopment\PhpStructure\Behaviour\Method;
 use NullDevelopment\PhpStructure\DataType\Constant;
 use NullDevelopment\PhpStructure\DataType\Property;
 use NullDevelopment\PhpStructure\DataTypeName\TraitName;
+use NullDevelopment\Skeleton\SourceCode;
 
 /**
  * @see TraitDefinitionSpec
  * @see TraitDefinitionTest
  */
-class TraitDefinition
+class TraitDefinition implements SourceCode
 {
     /** @var TraitName */
     private $name;
@@ -45,10 +46,46 @@ class TraitDefinition
         return $this->name;
     }
 
+    public function getNamespace(): ?string
+    {
+        return $this->name->getNamespace();
+    }
+
+    /**
+     * @TODO: this name is totally WRONG!
+     */
+    public function getClassName(): string
+    {
+        return $this->name->getName();
+    }
+
+    public function getFullName(): string
+    {
+        return $this->name->getFullName();
+    }
+
+    public function hasTraits(): bool
+    {
+        if (true === empty($this->traits)) {
+            return false;
+        }
+
+        return true;
+    }
+
     /** @return TraitName[] */
     public function getTraits(): array
     {
         return $this->traits;
+    }
+
+    public function hasConstants(): bool
+    {
+        if (true === empty($this->constants)) {
+            return false;
+        }
+
+        return true;
     }
 
     /** @return Constant[] */
@@ -57,10 +94,28 @@ class TraitDefinition
         return $this->constants;
     }
 
+    public function hasProperties(): bool
+    {
+        if (true === empty($this->properties)) {
+            return false;
+        }
+
+        return true;
+    }
+
     /** @return Property[] */
     public function getProperties(): array
     {
         return $this->properties;
+    }
+
+    public function hasMethods(): bool
+    {
+        if (true === empty($this->methods)) {
+            return false;
+        }
+
+        return true;
     }
 
     /** @return Method[] */
