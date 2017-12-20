743b1607372cac5e5807daaf6f5ca10ac0129fb4
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 16:36:02 +0100

    Add SfTestCase

---
+++ tests/TestCase/SfTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\TestCase;
+
+use Symfony\Component\Yaml\Yaml;
+use tests\NullDev\ContainerSupportedTestCase;
+
+/** @SuppressWarnings("PHPMD.NumberOfChildren") */
+abstract class SfTestCase extends ContainerSupportedTestCase
+{
+    protected function loadDefinitionYaml(string $fileName, ?string $path = null): array
+    {
+        // Define path
+        if (null === $path) {
+            $path = getcwd().'/definitions/'.$fileName;
+        } else {
+            $path .= '/'.$fileName;
+        }
+
+        // Load Yaml file content
+        $fileContent = file_get_contents($path);
+
+        // Parse content
+        $parsedYaml = Yaml::parse($fileContent);
+
+        // Return definition part.
+        return $parsedYaml['definition'];
+    }
+}
