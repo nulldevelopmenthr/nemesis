d377f53218dd663a4d950e02a50e01beb8736165
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 21:40:31 +0100

    Generate missing tests

---
+++ tests/NullDevelopment/SkeletonNetteGenerator/SourceCode/Middleware/InterfaceMiddlewareTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tests\NullDevelopment\SkeletonNetteGenerator\SourceCode\Middleware;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDevelopment\SkeletonNetteGenerator\SourceCode\Middleware\InterfaceMiddleware;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\SkeletonNetteGenerator\SourceCode\Middleware\InterfaceMiddleware
+ * @group todo
+ */
+class InterfaceMiddlewareTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var InterfaceMiddleware */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = new InterfaceMiddleware();
+    }
+
+    public function testExecute()
+    {
+        $this->markTestSkipped('Skipping');
+    }
+}
