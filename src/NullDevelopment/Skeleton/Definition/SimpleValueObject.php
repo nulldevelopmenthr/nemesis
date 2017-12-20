b4de66db8725fa8615c1e23e31a1d8c281ed6dd3
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 10:20:30 +0100

    Add SimpleValueObject

---
+++ src/NullDevelopment/Skeleton/Definition/SimpleValueObject.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton\Definition;
+
+use NullDevelopment\PhpStructure\Type\ClassType;
+use NullDevelopment\Skeleton\SourceCode;
+
+/**
+ * @see SimpleValueObjectSpec
+ * @see SimpleValueObjectTest
+ */
+class SimpleValueObject extends ClassType implements SourceCode
+{
+}
