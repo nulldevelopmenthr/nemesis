2f49206499511d2ef0a8b881f4844651fb8ac870
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 16:40:10 +0100

    Add Nulldevelopment\Skeleton

---
+++ src/NullDevelopment/Skeleton/ObjectConfigurationLoaderCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton;
+
+use NullDevelopment\PhpStructure\Type\ClassType;
+use Webmozart\Assert\Assert;
+
+class ObjectConfigurationLoaderCollection
+{
+    /** @var array */
+    private $loaders;
+
+    public function __construct(array $loaders = [])
+    {
+        Assert::allIsInstanceOf($loaders, DefinitionConfigurationLoader::class);
+        $this->loaders = $loaders;
+    }
+
+    public function getLoaders(): array
+    {
+        return $this->loaders;
+    }
+
+    public function findAndLoad(array $config): ClassType
+    {
+        foreach ($this->loaders as $loader) {
+            if (true === $loader->supports($config)) {
+                return $loader->load($config);
+            }
+        }
+    }
+}
---
+++ src/NullDevelopment/Skeleton/SkeletonExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton;
+
+use NullDevelopment\Skeleton\CompilerPass\ObjectConfigurationLoaderCompilerPass;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class SkeletonExtension extends Extension
+{
+    /** @SuppressWarnings(PHPMD.UnusedFormalParameter) */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $container->registerForAutoconfiguration(DefinitionConfigurationLoader::class)
+            ->addTag('skeleton.object_configuration_loader');
+
+        $container->addCompilerPass(new ObjectConfigurationLoaderCompilerPass());
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/Resources/'));
+        $loader->load('services.yml');
+        $loader->load('cli.yml');
+    }
+}
