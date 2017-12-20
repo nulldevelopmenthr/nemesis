19ed68ef5e02f161185bcc225c644428d0869b48
Miro Svrtan <miro@mirosvrtan.me>
2017-12-20 16:39:22 +0100

    Add SimpleVariable

---
+++ src/NullDevelopment/PhpStructure/DataType/SimpleVariable.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\PhpStructure\DataType;
+
+use NullDevelopment\PhpStructure\DataTypeName\ContractName;
+
+/**
+ * @see MethodParameterSpec
+ * @see MethodParameterTest
+ */
+class SimpleVariable implements Variable
+{
+    /** @var string */
+    private $name;
+    /** @var ContractName */
+    private $contractName;
+
+    public function __construct(string $name, ContractName $contractName)
+    {
+        $this->name          = $name;
+        $this->contractName  = $contractName;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getInstanceName(): ContractName
+    {
+        return $this->contractName;
+    }
+
+    public function getInstanceFullName(): string
+    {
+        return $this->contractName->getFullName();
+    }
+}
