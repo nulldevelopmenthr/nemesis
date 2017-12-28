983f224a40c4c4d8ec2baad8116aa7e9bb6de162
Miro Svrtan <miro@mirosvrtan.me>
2017-12-28 23:55:21 +0100

    PhpUnitSpecification should define getSubjectUnderTest()

--- src/NullDevelopment/Skeleton/PhpUnitSpecification.php
+++ src/NullDevelopment/Skeleton/PhpUnitSpecification.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace NullDevelopment\Skeleton;
 
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+
 interface PhpUnitSpecification
 {
+    public function getSubjectUnderTest(): ClassName;
 }
