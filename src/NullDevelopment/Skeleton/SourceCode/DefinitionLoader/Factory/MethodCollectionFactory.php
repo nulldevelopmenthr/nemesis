1821f05d77f7828d918899f8b6352798a9a93528
Miro Svrtan <miro@mirosvrtan.me>
2018-01-01 20:48:33 +0100

    Feature #349: Add support for generating getMethods in MethodCollectionFactory

--- src/NullDevelopment/Skeleton/SourceCode/DefinitionLoader/Factory/MethodCollectionFactory.php
+++ src/NullDevelopment/Skeleton/SourceCode/DefinitionLoader/Factory/MethodCollectionFactory.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace NullDevelopment\Skeleton\SourceCode\DefinitionLoader\Factory;
 
+use Exception;
+use NullDevelopment\PhpStructure\DataType\Property;
 use NullDevelopment\PhpStructure\DataType\Visibility;
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+use NullDevelopment\Skeleton\SourceCode\Method\GetterMethod;
 
 /**
  * @see MethodCollectionFactorySpec
@@ -20,17 +24,31 @@ class MethodCollectionFactory
 
         $result = [];
 
+        foreach ($input as $methodName => $methodInput) {
+            if ('getter' === $methodInput['type']) {
+                $property = new Property(
+                    $methodInput['property']['name'],
+                    ClassName::create($methodInput['property']['instanceOf']),
+                    $methodInput['property']['nullable'],
+                    $methodInput['property']['hasDefault'],
+                    $methodInput['property']['default'],
+                    new Visibility('private')
+                );
+
+                $result[] = new GetterMethod($methodName, $property);
+            } else {
+                throw new Exception('ERR 322315002: Only getter is implemented for now!');
+            }
+        }
+
         return $result;
     }
 
     protected function getDefaultValues()
     {
         return [
-            'instanceOf' => 'int',
-            'nullable'   => false,
-            'hasDefault' => false,
-            'default'    => null,
-            'visibility' => Visibility::PUBLIC,
+            'type'       => 'getter',
+            'parameters' => [],
         ];
     }
 }
