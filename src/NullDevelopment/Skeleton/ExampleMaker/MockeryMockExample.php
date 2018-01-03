c9e90c6b72cb817eb2693482986af7e5eed15605
Miro Svrtan <miro@mirosvrtan.me>
2018-01-03 01:15:11 +0100

    ExampleMaker will return MockeryMockExample when hits interface

---
+++ src/NullDevelopment/Skeleton/ExampleMaker/MockeryMockExample.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton\ExampleMaker;
+
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+use NullDevelopment\PhpStructure\DataTypeName\ContractName;
+
+/**
+ * @see InstanceExampleSpec
+ * @see InstanceExampleTest
+ */
+class MockeryMockExample implements Example
+{
+    /** @var ContractName */
+    private $instanceOf;
+
+    public function __construct(ContractName $instanceOf)
+    {
+        $this->instanceOf = $instanceOf;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('\Mockery::mock(\'%s\')', $this->instanceOf->getFullName());
+    }
+
+    public function classesToImport(): array
+    {
+        return [
+            ClassName::create('Mockery'),
+            $this->instanceOf,
+        ];
+    }
+}
