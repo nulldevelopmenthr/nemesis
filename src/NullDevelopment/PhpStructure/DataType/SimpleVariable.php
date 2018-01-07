2da600c5c432aeb4cd54a921d90e11c6d1163c73
Miro Svrtan <miro@mirosvrtan.me>
2018-01-07 20:06:11 +0100

    Feat #485: Add simple factory methods on SimpleVariable

--- src/NullDevelopment/PhpStructure/DataType/SimpleVariable.php
+++ src/NullDevelopment/PhpStructure/DataType/SimpleVariable.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace NullDevelopment\PhpStructure\DataType;
 
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
 use NullDevelopment\PhpStructure\DataTypeName\ContractName;
 
 /**
@@ -22,6 +23,31 @@ class SimpleVariable implements Variable
     {
         $this->name         = $name;
         $this->contractName = $contractName;
+    }
+
+    public static function int(string $name): self
+    {
+        return new self($name, ClassName::create('int'));
+    }
+
+    public static function string(string $name): self
+    {
+        return new self($name, ClassName::create('string'));
+    }
+
+    public static function float(string $name): self
+    {
+        return new self($name, ClassName::create('float'));
+    }
+
+    public static function bool(string $name): self
+    {
+        return new self($name, ClassName::create('bool'));
+    }
+
+    public static function array(string $name): self
+    {
+        return new self($name, ClassName::create('array'));
     }
 
     public function getName(): string
